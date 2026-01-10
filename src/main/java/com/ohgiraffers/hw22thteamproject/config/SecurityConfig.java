7c45032fe67167a7d12dcbdd2233ee16149fe984
sinwooKim <rlatlsdn98@naver.com>
2026-01-11 02:32:01 +0900

    Security Config 401 Error Bug 수정

--- src/main/java/com/ohgiraffers/hw22thteamproject/config/SecurityConfig.java
+++ src/main/java/com/ohgiraffers/hw22thteamproject/config/SecurityConfig.java
@@ -56,6 +56,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth ->
                         auth
                                 .requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
+                                .requestMatchers("/error").permitAll()
                                 // 임시: 모든 요청 허용 (테스트용)
                                 .anyRequest().authenticated()
                 )
