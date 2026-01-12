fe0d46dd012552e48ae6e2132cb6a1c65c5b0437
gusgh075 <gusgh075@gmail.com>
2026-01-12 23:22:01 +0900

    chore: DishCategory 관련 jparepository파일 삭제. DishCategory테이블은 불변성을 유지해야함

--- src/main/java/com/ohgiraffers/hw22thteamproject/recipe/command/infrastructure/repository/JpaDishCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ohgiraffers.hw22thteamproject.recipe.command.infrastructure.repository;
-
-import com.ohgiraffers.hw22thteamproject.recipe.command.domain.aggregate.DishCategory;
-import com.ohgiraffers.hw22thteamproject.recipe.command.domain.repository.DishCategoryRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaDishCategoryRepository extends DishCategoryRepository, JpaRepository<DishCategory, Integer> {
-}
