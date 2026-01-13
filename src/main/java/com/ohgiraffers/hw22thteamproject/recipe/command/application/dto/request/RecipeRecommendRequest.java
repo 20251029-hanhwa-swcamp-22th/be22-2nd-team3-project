a3ebcbf8c2699335fce16532e324642b3393ab64
gusgh075 <gusgh075@gmail.com>
2026-01-13 19:51:30 +0900

    fix: foodTypes를 List->Set, String->DishCategoryEnum으로 제한함

--- src/main/java/com/ohgiraffers/hw22thteamproject/recipe/command/application/dto/request/RecipeRecommendRequest.java
+++ src/main/java/com/ohgiraffers/hw22thteamproject/recipe/command/application/dto/request/RecipeRecommendRequest.java
@@ -1,6 +1,10 @@
 package com.ohgiraffers.hw22thteamproject.recipe.command.application.dto.request;
 
 import java.util.List;
+import java.util.Set;
+
+import com.ohgiraffers.hw22thteamproject.recipe.command.application.dto.DishCategoryEnum;
+
 import lombok.*;
 
 @Getter
@@ -14,7 +18,7 @@ public class RecipeRecommendRequest {
   private List<String> dislikedIngredients;   // 못 먹는 재료
   private CookingLevel skillLevel;            // 요리 숙련도
   private List<String> tools;                 // 보유 조리 도구
-  private List<String> foodTypes;             // 원하는 음식 유형 (최대 3개)
+  private Set<DishCategoryEnum> foodTypes;             // 원하는 음식 유형 (최대 3개)
   private String preference;                  // 취향
   private int servings;                       // 몇 인분
 
