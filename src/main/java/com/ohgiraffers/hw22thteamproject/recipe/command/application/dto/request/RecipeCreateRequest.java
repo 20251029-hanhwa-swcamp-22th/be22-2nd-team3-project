94b21c9e707d2c25d31d26a8ea1da0fabe2176b5
gusgh075 <gusgh075@gmail.com>
2026-01-13 19:50:30 +0900

    fix: dishCategoryNo를 Enum타입으로 명시함

--- src/main/java/com/ohgiraffers/hw22thteamproject/recipe/command/application/dto/request/RecipeCreateRequest.java
+++ src/main/java/com/ohgiraffers/hw22thteamproject/recipe/command/application/dto/request/RecipeCreateRequest.java
@@ -1,22 +1,22 @@
 package com.ohgiraffers.hw22thteamproject.recipe.command.application.dto.request;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import java.util.List;
 
+import com.ohgiraffers.hw22thteamproject.recipe.command.application.dto.DishCategoryEnum;
+
 @Getter
 @Setter
 public class RecipeCreateRequest {
     @NotBlank(message = "요리 이름은 필수입니다.")
-    private String dishName;
-    
-    @NotBlank(message = "이미지 경로는 필수입니다.")
-    private String dishImgFileRoute;
+    private String dishNo;
     
     @NotNull(message = "카테고리 번호는 필수입니다.")
-    private Integer dishCategoryNo;
+    private DishCategoryEnum dishCategoryEnum;
 
     @NotNull(message = "사용자 아이디는 필수입니다.")
     private String userId;
