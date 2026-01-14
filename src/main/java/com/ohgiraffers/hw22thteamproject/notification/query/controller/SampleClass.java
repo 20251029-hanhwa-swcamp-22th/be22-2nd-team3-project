575447a2c04bdd61e7f54a5da9feb1b9fbe3985c
sinwooKim <rlatlsdn98@naver.com>
2026-01-14 21:37:51 +0900

    list user_no mapping notifications

--- src/main/java/com/ohgiraffers/hw22thteamproject/notification/query/controller/SampleClass.java
+++
@@ -1,4 +0,0 @@
-package com.ohgiraffers.hw22thteamproject.notification.query.controller;
-
-public class SampleClass {
-}
--- src/main/java/com/ohgiraffers/hw22thteamproject/notification/query/mapper/NotificationMapper.java
+++ src/main/java/com/ohgiraffers/hw22thteamproject/notification/query/mapper/NotificationMapper.java
@@ -1,8 +1,12 @@
 package com.ohgiraffers.hw22thteamproject.notification.query.mapper;
 
+import com.ohgiraffers.hw22thteamproject.notification.query.dto.response.NotificationDTO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface NotificationMapper {
 
+    List<NotificationDTO> getNotificationsByUserNo(long userNo);
 }
